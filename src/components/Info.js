b20d70f26dfa730bf39fbe5f98a74c6b7e1e3207
Jeremy Snyder <jeremy.snyder@gmail.com>
2019-10-31 21:53:40 -0400

    Updated version and about statement

--- src/components/Info.js
+++ src/components/Info.js
@@ -7,12 +7,15 @@ export function Info(props) {
   return (
     <Paper style={{ margin: 10, padding: 10 }}>
       <Typography variant="h5" component="h3">
-        Read Daily v1.0.0
+        Read Daily v1.0.1
       </Typography>
       <Typography component="p">
-        Read Daily provides chapter-based readings from the Two-Year Bible Reading Plan, produced jointly by
-        Reformation OPC (Queens, NYC) and Resurrection OPC (State College, PA).
-        For more resources, visit <a href="http://resurrectionopc.org/resources" target="_blank" rel="noopener noreferrer">resurrectionopc.org/resources</a>.
+        Read Daily is a free service produced in the hopes
+        of facilitating sustained Bible reading in a mobile
+        society. The OT readings are arranged chronologically,
+        configurable to a 1 or 2 year cycle. The NT and Psalms
+        are read in their entirety each year, with the Gospels,
+        Acts, and Proverbs repeated twice.
       </Typography>
     </Paper>
   )
